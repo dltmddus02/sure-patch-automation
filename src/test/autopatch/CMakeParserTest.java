66b9b22106dbf91008685c2924a44508d59928d7
dltmddus <tmddus4671@gmail.com>
2024-10-28 16:11:07 +0900

    fix: 테스트용 CMakeLists.txt 추가, 정적 경로로 수정

--- src/test/autopatch/CMakeParserTest.java
+++ src/test/autopatch/CMakeParserTest.java
@@ -32,50 +32,65 @@ class CMakeParserTest {
 		parser = cmakeParser.new Parser();
 		moduleProcessor = new ModuleProcessor();
 
-		String rootPath = "C:\\Users\\sure\\test\\engine";
+		String rootPath = "src\\test\\engine";
 		root = preprocessor.preprocess(rootPath);
 	}
 
 	@Test
-	@DisplayName("output 타입이 EXE인 경우")
+	@DisplayName("EXE인 경우 모듈 정보 추출 검증")
 	void processAddExecutableTest() {
 		List<Module> modules = new ArrayList<>();
 
-		String line = "add_executable(${PROJECT_NAME} ${SRC_PROJECT})";
+		String line = "add_executable(Foundation ${SRC_PROJECT})";
 
 		moduleProcessor.processAddExecutable(line, modules);
+
+		String expectedModuleName = "Foundation";
+		String actualModuleName = modules.get(0).getModuleName();
 
 		String expectedModuleType = "EXE";
 		String actualModuleType = modules.get(0).getOutputType();
 
+		assertEquals(expectedModuleName, actualModuleName);
 		assertEquals(expectedModuleType, actualModuleType);
 	}
 
 	@Test
-	@DisplayName("output 타입이 shared library인 경우")
+	@DisplayName("shared library인 경우 모듈 정보 추출 검증")
 	void processAddLibrarySHAREDTest() {
 		List<Module> modules = new ArrayList<>();
 
-		String line = "add_library(${PROJECT_NAME} SHARED ${SRC_PROJECT} ${HEADER_FILES})";
+		String line = "add_library(Foundation SHARED ${SRC_PROJECT} ${HEADER_FILES})";
 
 		moduleProcessor.processAddLibrary(line, modules);
+
+		String expectedModuleName = "Foundation";
+		String actualModuleName = modules.get(0).getModuleName();
 
 		String expectedModuleType = "SHARED";
 		String actualModuleType = modules.get(0).getOutputType();
 
+		assertEquals(expectedModuleName, actualModuleName);
 		assertEquals(expectedModuleType, actualModuleType);
 	}
 
 	@Test
-	@DisplayName("output 타입이 static library인 경우")
+	@DisplayName("static library인 경우 모듈 정보 추출 검증")
 	void processAddLibrarySTATICTest() {
 		List<Module> modules = new ArrayList<>();
 
 		String line = "add_library(${PROJECT_NAME} STATIC ${SRC_PROJECT} ${HEADER_FILES})";
 
 		moduleProcessor.processAddLibrary(line, modules);
-		
+
 		assertTrue(modules.isEmpty(), "STATIC 라이브러리는 고려하지 않습니다.");
+	}
+
+	@Test
+	@DisplayName("의존 모듈 정보 추출 검증")
+	void processTargetLinkLibrariesTest() {
+		String line = "add_executable(${PROJECT_NAME} ${SRC_PROJECT})";
+
 	}
 
 }
--- src/test/autopatch/CMakePreprocessorTest.java
+++ src/test/autopatch/CMakePreprocessorTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -27,31 +29,35 @@ class CMakePreprocessorTest {
 	@Test
 	@DisplayName("루트디렉토리_전처리_테스트")
 	void rootPreprocessorTest() throws IOException {
-		String rootPath = "C:\\Users\\sure\\test\\engine";
-		CMakeContents root = preprocessor.preprocess(rootPath);
+	    String rootPath = "src\\test\\engine";
+	    // 현재 디렉토리 뒤에 rootPath를 붙이는 코드
+	    Path fullPath = Paths.get("").toAbsolutePath().resolve(rootPath);
+	    
+	    CMakeContents root = preprocessor.preprocess(fullPath.toString());
 
-		assertNotNull(root);
-		assertEquals(rootPath, root.path);
-		assertFalse(root.contents.isEmpty());
+	    assertNotNull(root);
+	    assertEquals(fullPath.toString(), root.path);
+	    assertFalse(root.contents.isEmpty());
+	    
 	}
 
 	@Test
 	@DisplayName("하위디렉토리_전처리_테스트")
 	void subdirectoryPreprocessorTest() throws IOException {
-		CMakeContents child1 = preprocessor.preprocess("C:\\Users\\sure\\test\\engine\\sub1");
+		CMakeContents child1 = preprocessor.preprocess("src\\test\\engine\\sub1");
 		assertNotNull(child1);
-		assertEquals("C:\\Users\\sure\\test\\engine\\sub1", child1.path);
+		assertEquals("src\\test\\engine\\sub1", child1.path);
 		assertFalse(child1.contents.isEmpty());
 
-		CMakeContents child2 = preprocessor.preprocess("C:\\Users\\sure\\test\\engine\\sub2");
+		CMakeContents child2 = preprocessor.preprocess("src\\test\\engine\\sub2");
 		assertNotNull(child2);
-		assertEquals("C:\\Users\\sure\\test\\engine\\sub2", child2.path);
+		assertEquals("src\\test\\engine\\sub2", child2.path);
 	}
 
 	@Test
 	@DisplayName("매크로_치환_테스트")
 	void macroReplacementTest() throws IOException {
-		String rootPath = "C:\\Users\\sure\\test\\engine";
+		String rootPath = "src\\test\\engine";
 		CMakeContents root = preprocessor.preprocess(rootPath);
 
 		String expectedMacroValue = "매크로";
@@ -61,7 +67,7 @@ class CMakePreprocessorTest {
 	@Test
 	@DisplayName("하위디렉토리_매크로_치환_테스트")
 	void subdirectoryMacroReplacementTest() throws IOException {
-		CMakeContents root = preprocessor.preprocess("C:\\Users\\sure\\test\\engine");
+		CMakeContents root = preprocessor.preprocess("src\\test\\engine");
 		CMakeContents child3 = root.children.get(2);
 		CMakeContents child3_2 = child3.children.get(1);
 		CMakeContents child3_2_1 = child3_2.children.getFirst();
@@ -73,13 +79,13 @@ class CMakePreprocessorTest {
 	@Test
 	@DisplayName("하위디렉토리_구조_테스트")
 	void subdirectoryStructureTest() throws IOException {
-		CMakeContents root = preprocessor.preprocess("C:\\Users\\sure\\test\\engine");
+		CMakeContents root = preprocessor.preprocess("src\\test\\engine");
 		//
 		assertEquals(4, root.children.size());
 
 		CMakeContents child3 = root.children.get(2);
 
-		assertEquals("C:\\Users\\sure\\test\\engine\\sub3", child3.path);
+		assertEquals("src\\test\\engine\\sub3", child3.path);
 		assertEquals(2, child3.children.size());
 	}
 
