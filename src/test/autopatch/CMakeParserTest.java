f8ca8aaea706ac198ff34ae8decb6a504a17348e
dltmddus <tmddus4671@gmail.com>
2024-10-28 14:58:17 +0900

    feat: 모듈 output 정보 추출 검증 테스트 코드 구현

--- src/test/autopatch/CMakeParserTest.java
+++ src/test/autopatch/CMakeParserTest.java
@@ -1,5 +1,8 @@
 package test.autopatch;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,24 +10,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import main.autopatch.Module;
 import main.autopatch.CMakeParser;
 import main.autopatch.CMakeParser.Parser;
 import main.autopatch.CMakePreprocessor;
 import main.autopatch.CMakePreprocessor.CMakeContents;
 import main.autopatch.CMakePreprocessor.Preprocessor;
-import main.util.ValidationUtil;
+import main.service.ModuleProcessor;
 
 class CMakeParserTest {
 	private Preprocessor preprocessor;
 	private CMakeParser cmakeParser;
 	private Parser parser;
 	private CMakeContents root;
+	private ModuleProcessor moduleProcessor;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		preprocessor = new CMakePreprocessor().new Preprocessor();
 		cmakeParser = new CMakeParser();
 		parser = cmakeParser.new Parser();
+		moduleProcessor = new ModuleProcessor();
 
 		String rootPath = "C:\\Users\\sure\\test\\engine";
 		root = preprocessor.preprocess(rootPath);
@@ -34,13 +40,42 @@ class CMakeParserTest {
 	@DisplayName("output 타입이 EXE인 경우")
 	void processAddExecutableTest() {
 		List<Module> modules = new ArrayList<>();
-		
+
 		String line = "add_executable(${PROJECT_NAME} ${SRC_PROJECT})";
 
-//		processAddExecutable(line, modules);
+		moduleProcessor.processAddExecutable(line, modules);
 
-		String path = "C:\\Users\\sure\\test\\engine\\sub1";
+		String expectedModuleType = "EXE";
+		String actualModuleType = modules.get(0).getOutputType();
 
+		assertEquals(expectedModuleType, actualModuleType);
+	}
+
+	@Test
+	@DisplayName("output 타입이 shared library인 경우")
+	void processAddLibrarySHAREDTest() {
+		List<Module> modules = new ArrayList<>();
+
+		String line = "add_library(${PROJECT_NAME} SHARED ${SRC_PROJECT} ${HEADER_FILES})";
+
+		moduleProcessor.processAddLibrary(line, modules);
+
+		String expectedModuleType = "SHARED";
+		String actualModuleType = modules.get(0).getOutputType();
+
+		assertEquals(expectedModuleType, actualModuleType);
+	}
+
+	@Test
+	@DisplayName("output 타입이 static library인 경우")
+	void processAddLibrarySTATICTest() {
+		List<Module> modules = new ArrayList<>();
+
+		String line = "add_library(${PROJECT_NAME} STATIC ${SRC_PROJECT} ${HEADER_FILES})";
+
+		moduleProcessor.processAddLibrary(line, modules);
+		
+		assertTrue(modules.isEmpty(), "STATIC 라이브러리는 고려하지 않습니다.");
 	}
 
 }
