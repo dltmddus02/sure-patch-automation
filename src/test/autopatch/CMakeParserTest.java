f4d23fe7df7d794fb06ecd1c7d070a3da34bd037
dltmddus <tmddus4671@gmail.com>
2024-10-29 16:28:00 +0900

    test: 의존 모듈 정보 추출 검증 테스트 세부화

--- src/test/autopatch/CMakeParserTest.java
+++ src/test/autopatch/CMakeParserTest.java
@@ -2,26 +2,34 @@ package test.autopatch;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import main.autopatch.Module;
 import main.autopatch.CMakeParser;
-import main.autopatch.CMakeParser.Parser;
 import main.autopatch.CMakePreprocessor;
 import main.autopatch.CMakePreprocessor.CMakeContents;
 import main.autopatch.CMakePreprocessor.Preprocessor;
+import main.autopatch.Module;
+import main.domain.Condition;
 import main.service.ModuleProcessor;
 
 class CMakeParserTest {
 	private Preprocessor preprocessor;
 	private CMakeParser cmakeParser;
-	private Parser parser;
+	private CMakeParser.Parser parser;
+	private Condition condition;
+//	private Parser parser;
 	private CMakeContents root;
 	private ModuleProcessor moduleProcessor;
 
@@ -29,7 +37,8 @@ class CMakeParserTest {
 	void setUp() throws Exception {
 		preprocessor = new CMakePreprocessor().new Preprocessor();
 		cmakeParser = new CMakeParser();
-		parser = cmakeParser.new Parser();
+		parser = new CMakeParser().new Parser();
+		condition = new Condition();
 		moduleProcessor = new ModuleProcessor();
 
 		String rootPath = "src\\test\\engine";
@@ -87,10 +96,63 @@ class CMakeParserTest {
 	}
 
 	@Test
-	@DisplayName("의존 모듈 정보 추출 검증")
-	void processTargetLinkLibrariesTest() {
-		String line = "add_executable(${PROJECT_NAME} ${SRC_PROJECT})";
+	@DisplayName("Condition if 정보 정상적으로 push되는지 검증")
+	public void testStoreConditionIFTest() throws Exception {
+		Method method = CMakeParser.Parser.class.getDeclaredMethod("storeConditionInfo", String.class, Condition.class);
+		method.setAccessible(true);
 
+		String ifLine = "if(CONDITION_A)";
+		method.invoke(parser, ifLine, condition);
+
+		assertEquals(1, condition.getData().size());
+		assertEquals("CONDITION_A", condition.getData().peek());
+	}
+
+	@Test
+	@DisplayName("Condition elseif 정보 정상적으로 push되는지 검증")
+	public void testStoreConditionElseIfTest() throws Exception {
+		Method method = CMakeParser.Parser.class.getDeclaredMethod("storeConditionInfo", String.class, Condition.class);
+		method.setAccessible(true);
+
+		String elseifLine = "elseif(CONDITION_A)";
+		method.invoke(parser, elseifLine, condition);
+
+		assertEquals(1, condition.getData().size());
+		assertEquals("CONDITION_A", condition.getData().peek());
+	}
+
+	@Test
+	@DisplayName("Condition endif/else 정상적으로 pop되는지 검증")
+	public void testStoreConditionEndIfTest() throws Exception {
+		condition.push("CONDITION_PRE");
+		
+		Method method = CMakeParser.Parser.class.getDeclaredMethod("storeConditionInfo", String.class, Condition.class);
+		method.setAccessible(true);
+
+		String elseLine = "else()";
+		method.invoke(parser, elseLine, condition);
+		
+		assertEquals(1, condition.getData().size());
+
+		String endifLine = "endif()";
+		method.invoke(parser, endifLine, condition);
+
+		assertEquals(0, condition.getData().size());
+	}
+
+	@Test
+	@DisplayName("의존 모듈 정보 추출 검증")
+	void processTargetLinkLibrariesTest() throws IOException {
+	    String line = "target_link_libraries(testName PRIVATE -Wl,--start-group cs_common libUTIL_LIB64 PocoFoundationmt ucommon -Wl,--end-group ${CMAKE_DL_LIBS} )";
+
+	    Module module = new Module(new StringBuilder("testName"), "EXE");
+	    List<Module> modules = Arrays.asList(module);
+	    
+	    condition.push("UNIX");
+ 
+	    moduleProcessor.processTargetLinkLibraries(line, condition, modules);
+	    
+	    Assertion.assertThat(module.addAffectedModule("testName")).contains("PocoFoundationmt");
 	}
 
 }
