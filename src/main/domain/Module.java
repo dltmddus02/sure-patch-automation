bd392d42db0916d4227ea52d4bc41a62735b68fe
dltmddus <tmddus4671@gmail.com>
2024-11-14 13:28:10 +0900

    fix: isTopModule 클래스 변수 추가
    소스파일로부터 참조되는 모듈 추출할 때 top모듈만 추출하기 위함.
    
    m1 ->  참조 모듈 : m2, m3, m4 참조 소스파일 : a.cpp b.cpp a.h (즉 m2,m3,m4 있어야 m1 빌드됨)
    m2 ->  참조 모듈 : m5, m6 참조 소스파일 : c.cpp b.h
    
    -> 소스파일 c.cpp 변경되면, m1 추출되어야 함. m2는 추출 X.

--- src/main/domain/Module.java
+++ src/main/domain/Module.java
@@ -10,12 +10,14 @@ public class Module {
 	private String outputType;
 	private Set<String> affectedModules;
 	private List<String> sourceFiles;
+	private boolean isTopModule;
 
 	public Module(StringBuilder moduleName, String outputType) {
 		this.moduleName = moduleName;
 		this.outputType = outputType;
 		this.setAffectedModules(new HashSet<>());
 		this.setSourceFiles(new ArrayList<>());
+		this.isTopModule = true;
 	}
 
 	public String getModuleName() {
@@ -26,12 +28,20 @@ public class Module {
 		return outputType;
 	}
 
+	public void setOutputType(String outputType) {
+		this.outputType = outputType;
+	}
+
 	public Set<String> getAffectedModules() {
 		return affectedModules;
 	}
 
 	public void setAffectedModules(Set<String> affectedModules) {
 		this.affectedModules = affectedModules;
+	}
+
+	public List<String> getSourceFiles() {
+		return sourceFiles;
 	}
 
 	public void setSourceFiles(ArrayList<String> sourceFiles) {
@@ -43,6 +53,42 @@ public class Module {
 	}
 
 	public void addSourceFile(String sourceFile) {
+		if (isNullOrEmpty(sourceFile)) {
+			return;
+		}
 		sourceFiles.add(sourceFile);
 	}
+
+	private boolean isNullOrEmpty(String sourceFile) {
+		if (sourceFile == null || sourceFile.isBlank())
+			return true;
+		return false;
+	}
+
+	public boolean getIsTopModule() {
+		return isTopModule;
+	}
+
+	public void setIsTopModule() {
+		isTopModule = false;
+	}
+
+	public Set<String> getAffectedModulesByModuleName(String moduleName) {
+		if (this.moduleName.toString().equals(moduleName)) {
+			return affectedModules;
+		}
+		return new HashSet<>();
+	}
+
+	public static void printAllModuleNamesAndReferences(List<Module> modules) {
+		for (Module module : modules) {
+			System.out.println("모듈이름 : " + module.getModuleName());
+			System.out.println("참조받는 모듈 : " + module.getAffectedModules());
+			System.out.println("모듈타입 : " + module.getOutputType());
+			System.out.println("top모듈인가 : " + module.getIsTopModule());
+			System.out.println("소스파일 : " + module.getSourceFiles());
+			System.out.println("---------");
+		}
+	}
+
 }
